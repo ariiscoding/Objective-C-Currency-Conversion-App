fada7df156fd7514d1c40051b5fa5773a164cb4a
Ari He <ariqhe@gmail.com>
2021-08-01 20:24:24 -0700

    Added an output view controller

---
+++ Objective C Currency Conversion App/View Controllers/Current Output/CurrencyOutputViewController.h
@@ -0,0 +1,20 @@
+//
+//  CurrencyOutputViewController.h
+//  CurrencyOutputViewController
+//
+//  Created by Ari He on 8/1/21.
+//
+
+#ifndef CurrencyOutputViewController_h
+#define CurrencyOutputViewController_h
+
+#import <UIKit/UIKit.h>
+
+@interface CurrencyOutputViewController : UIViewController
+
+- (void)setDollarValueTo:(double)newValue;
+
+@end
+
+
+#endif /* CurrencyOutputViewController_h */
---
+++ Objective C Currency Conversion App/View Controllers/Current Output/CurrencyOutputViewController.m
@@ -0,0 +1,33 @@
+//
+//  CurrencyOutputViewController.m
+//  CurrencyOutputViewController
+//
+//  Created by Ari He on 8/1/21.
+//
+
+#import "CurrencyOutputViewController.h"
+
+@implementation CurrencyOutputViewController
+
+// MARK: - Properties
+
+
+// MARK: - Lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+// MARK: - View Setups
+
+
+// MARK: - Constraints
+
+
+// MARK: - Intents
+
+
+// MARK: - Helper Methods
+
+
+@end
